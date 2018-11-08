b3508a64235f9415733e70fc2bfc300ed354b695
sstienface <s.stien@fondationface.org>
2018-11-08 11:14:55 +0100

    Initial Commit

---
+++ exercice1.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sstienface on 16/10/2018.
+ */
+
+var voiture = {
+    "Nom" : "Eggo",
+    "imgUrl" : "https://fr.ubergizmo.com/wp-content/uploads/2011/10/eggo_01.jpg",
+    "nombresRoues" : 4,
+    "Couleur" : "Marron",
+    "Contructeur" : "Citroen",
+    "Carburant" : "Solaire",
+    "nombrePortes" : 3,
+    "Autonomie" : "450km",
+    "vitesseMaxi" : "120km/h"
+};
+
+
+
+
