ce8493d98da0aaadd65553e691d5304b9a947155
Amandine <amandine.labaeye@orange.fr>
2018-11-15 14:03:46 +0100

    commit

--- exercice1.js
+++ exercice1.js
@@ -2,7 +2,7 @@
  * Created by sstienface on 16/10/2018.
  */
 
-var voiture = {
+let voiture = {
     "Nom" : "Eggo",
     "imgUrl" : "https://fr.ubergizmo.com/wp-content/uploads/2011/10/eggo_01.jpg",
     "nombresRoues" : 4,
@@ -14,6 +14,39 @@ var voiture = {
     "vitesseMaxi" : "120km/h"
 };
 
+document.getElementById("Titre").innerHTML = voiture.Nom + ": La voiture futuriste";
+
+let Image = document.getElementById("ImageV");
+
+Image.src = voiture.imgUrl;
+
+document.getElementById("Couleur").innerHTML = voiture.Couleur;
+
+document.getElementById("Roues").innerHTML = voiture.nombresRoues;
+
+document.getElementById("Constructeur").innerHTML = voiture.Contructeur;
+
+document.getElementById("Carburant").innerHTML = voiture.Carburant;
+
+document.getElementById("Autonomie").innerHTML = voiture.Autonomie;
+
+document.getElementById("Portes").innerHTML = voiture.nombrePortes;
+
+document.getElementById("Vitesse").innerHTML = voiture.vitesseMaxi;
+
+document.getElementById("TitreB").innerHTML = voiture.Nom + ": La nouvelle voiture exceptionnel"
+
+document.getElementById("Element").style.display = "none";
+
+document.getElementById("BoutonD").addEventListener("click", function () {
 
 
+    let Desc = document.getElementById("Element");
+    if (Desc.style.display === "block") {
+        Desc.style.display = "none";
+    } else {
+        Desc.style.display = "block";
+    }
 
+
+});
